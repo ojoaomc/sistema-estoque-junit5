4daa3d2c42b223089ffcbdd18411920f37cbcda0
Paiva <danpaiva@live.com>
2025-04-21 16:01:45 -0300

    feat: add metodo que testa nome do produto nulo ou vazio

--- sistema-estoque/src/test/java/com/example/danhpaiva/SistemaEstoqueTest.java
+++ sistema-estoque/src/test/java/com/example/danhpaiva/SistemaEstoqueTest.java
@@ -1,6 +1,7 @@
 package com.example.danhpaiva;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
@@ -10,15 +11,32 @@ public class SistemaEstoqueTest {
   private SistemaEstoque sistemaEstoque;
 
   @Before
-  void setUp() {
+  public void setUp() {
     sistemaEstoque = new SistemaEstoque();
   }
 
   @Test
-  void adicionarProduto_produtoNovo_adicionaAoEstoque() {
+  public void adicionarProduto_produtoNovo_adicionaAoEstoque() {
     sistemaEstoque.adicionarProduto("Camiseta", 10);
     assertEquals(10, sistemaEstoque.consultarEstoque("Camiseta"));
     assertEquals(1, sistemaEstoque.obterHistoricoTransacoes().size());
     assertEquals("Adicionado 10 unidade(s) de Camiseta", sistemaEstoque.obterHistoricoTransacoes().get(0));
+  }
+
+  @Test
+  public void adicionarProduto_produtoExistente_incrementaEstoque() {
+    sistemaEstoque.adicionarProduto("Calça", 5);
+    sistemaEstoque.adicionarProduto("Calça", 3);
+    assertEquals(8, sistemaEstoque.consultarEstoque("Calça"));
+    assertEquals(2, sistemaEstoque.obterHistoricoTransacoes().size());
+    assertEquals("Adicionado 5 unidade(s) de Calça", sistemaEstoque.obterHistoricoTransacoes().get(0));
+    assertEquals("Adicionado 3 unidade(s) de Calça", sistemaEstoque.obterHistoricoTransacoes().get(1));
+  }
+
+  @Test
+  void adicionarProduto_nomeNuloOuVazio_lancaIllegalArgumentException() {
+    assertThrows(IllegalArgumentException.class, () -> sistemaEstoque.adicionarProduto(null, 5));
+    assertThrows(IllegalArgumentException.class, () -> sistemaEstoque.adicionarProduto(" ", 5));
+    assertEquals(0, sistemaEstoque.obterHistoricoTransacoes().size());
   }
 }
