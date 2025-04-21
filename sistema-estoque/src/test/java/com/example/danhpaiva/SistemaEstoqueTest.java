6383dba95f93bc0c7cb6ddc4a82d82ea74b679c5
Paiva <danpaiva@live.com>
2025-04-21 15:57:11 -0300

    feat: add teste unitario para novo produto add ao estoque

--- sistema-estoque/src/test/java/com/example/danhpaiva/SistemaEstoqueTest.java
+++ sistema-estoque/src/test/java/com/example/danhpaiva/SistemaEstoqueTest.java
@@ -1,12 +1,24 @@
 package com.example.danhpaiva;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class SistemaEstoqueTest {
+  private SistemaEstoque sistemaEstoque;
+
+  @Before
+  void setUp() {
+    sistemaEstoque = new SistemaEstoque();
+  }
+
   @Test
-  public void shouldAnswerWithTrue() {
-    assertTrue(true);
+  void adicionarProduto_produtoNovo_adicionaAoEstoque() {
+    sistemaEstoque.adicionarProduto("Camiseta", 10);
+    assertEquals(10, sistemaEstoque.consultarEstoque("Camiseta"));
+    assertEquals(1, sistemaEstoque.obterHistoricoTransacoes().size());
+    assertEquals("Adicionado 10 unidade(s) de Camiseta", sistemaEstoque.obterHistoricoTransacoes().get(0));
   }
 }
